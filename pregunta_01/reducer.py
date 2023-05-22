d0e79ded2b552563a98602c4d6b4ebe514dbbd32
jorcor89 <125140181+jorcor89@users.noreply.github.com>
2023-05-22 09:30:10 -0500

    Pregunta 1
    Prueba

--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -1,3 +1,15 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+contar = {}
+
+for linea in sys.stdin:
+    if linea in contar:
+        contar[linea] = contar[linea] + 1
+    else:
+        contar[linea] = 1
+
+for item in contar.items():
+    print(item[0].rstrip() + '\t' + str(item[1]))
